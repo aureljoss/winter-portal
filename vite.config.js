7a203295940365b852b7c12df9bbbd98a9639f99
Aurelie <josserand.n.aurelie@gmail.com>
2025-01-11 19:00:06 -0500

    Test gh-pages and adds gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import glsl from 'vite-plugin-glsl'
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: './winter-portal',
     server:
     {
         host: true, // Open to local network and display URL
